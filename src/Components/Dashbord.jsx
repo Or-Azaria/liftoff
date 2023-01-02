a04b2b6ede319c7b28b775af355ce73510aeca47
avishay avraham <avishayavraham555@gmail.com>
2023-01-03 01:16:36 +0200

    dashbord

--- src/Components/Dashbord.jsx
+++ src/Components/Dashbord.jsx
@@ -13,16 +13,16 @@ export default function Dashbord() {
         
         backgroundImage:
         'url(' +
-        'https://media.istockphoto.com/id/1225155638/photo/green-leaves-on-white.jpg?b=1&s=612x612&w=0&k=20&c=U-yuGVnwjAnib3tiSZEiUA4rjimL0Nlfdwrkr7lpwQM=' +
+        'https://i.pinimg.com/564x/48/e4/6b/48e46b0e682500163de141554ebb2321.jpg' +
         ')',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         position: 'relative',
-        height:"300px"
+        height:"350px"
       }}
     >
     </div>
-    <p className="text-red">Hello</p>
+    {/* <p className="text-red">Hello</p> */}
     <div className="overflow-y-auto py-4 px-3 bg-gray-50 rounded dark:bg-gray-800">
       <ul className="underline underline-offset-1 space-y-2">
         <li className='cursor-pointer' onClick={()=>navigate('')}>
@@ -30,7 +30,7 @@ export default function Dashbord() {
             href="#"
             className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
           >
-            <span className=" ml-3">Junior Problem</span>
+            <span className=" ml-3">Need help?</span>
           </a>
         </li>
         <li className='cursor-pointer' onClick={()=>navigate('mentorhelp')}>
@@ -38,7 +38,7 @@ export default function Dashbord() {
             href="#"
             className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
           >
-            <span className="ml-3">Mentor Help</span>
+            <span className="ml-3">Help Junior</span>
           </a>
         </li>
         <li className='cursor-pointer' onClick={()=>navigate('profile')}>
