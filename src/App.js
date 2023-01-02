7e2754e226f15fa1b53a4ccc568e167c44ff5cca
aviva itzack <aviva8999@gmail.com>
2023-01-03 00:23:38 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import V from './pages/VolunteerForm'
-import J from './pages/JuniorForm'
-import C from './pages/CreateProfile'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-export default function App() {
+///////////////////
+import Login from "./pages/LogIn"
+import Register from "./pages/Register"
+import User from "./pages/UserDashbord"
+import Profile from "./pages/Profile"
+import JuniorProblem from "./pages/JuniorProblem"
+import MentorHelp from "./pages/MentorHelp"
+import System from "./pages/System"
+
+
+function App() {
   return (
-    <Routes>
-    <Route path="/" element={<C />}>
-      <Route
-        path="VolenteerForm"
-        element={<J />}
-      />
-      <Route path="JuniorForm" element={<V />} />
-    </Route>
-  </Routes>
-  )
+    <>
+    <Router>
+      <Routes>
+        {/* <Route path='login' element={<Login/>}/>
+        <Route path='register' element={<Register/>}/> */}
+
+
+        {/* routes for user */}
+        <Route path='/' element={<User/>}>
+          <Route path='' element={<JuniorProblem/>}/>
+          <Route path='mentorhelp' element={<MentorHelp/>}/>
+          <Route path='profile' element={<Profile/>}/>
+          <Route path='system' element={<System/>}/>
+        </Route>
+
+        {/* routes for volunteer */}
+        {/* <Route>
+          <Route path='juniortrack' element={<JuniorTrack/>}/>
+          <Route path='findmentor' element={<FindMentor/>}/>
+          <Route path='jobtrack' element={<JobTrack/>}/>
+          <Route path='system' element={<System/>}/>
+        </Route> */}
+      </Routes>
+    </Router>
+    </>
+  );
 }
 
+export default App;
