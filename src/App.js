382cd5fcea471a68108bb7628fd5ed8a4a156f37
aviva itzack <aviva8999@gmail.com>
2023-01-03 00:20:24 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import V from './pages/VolunteerForm'
+import J from './pages/JuniorForm'
+import C from './pages/CreateProfile'
 
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Routes>
+    <Route path="/" element={<C />}>
+      <Route
+        path="VolenteerForm"
+        element={<J />}
+      />
+      <Route path="JuniorForm" element={<V />} />
+    </Route>
+  </Routes>
+  )
 }
 
-export default App;
